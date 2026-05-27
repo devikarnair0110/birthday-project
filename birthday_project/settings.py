b0bc76128f20de4452c7534a30340433e43aebdb
devikarnair0110 <devikarnair0110@gmail.com>
2026-05-27 21:39:23 +0530

    Update ALLOWED_HOSTS to ['*']

--- birthday_project/settings.py
+++ birthday_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-swu+bw$(=pc+ij3&u^(xn8xrx72n_=)vc*!#vd-tkp=_%zen#o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
